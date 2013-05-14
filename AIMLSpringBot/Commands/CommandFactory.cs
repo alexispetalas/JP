5731e8b42a92a45f9dadb99e5e1821508f46ff43
peta <alexispetalas@gmail.com>
2013-05-14 18:44:31 -0300

    Move

---
+++ AIMLSpringBot/Commands/CommandFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace AIMLSpringBot.Commands
+{
+    public class CommandFactory
+    {
+        public static ICommand GetCommand(string name)
+        {
+            name = name.Remove(name.Length - 1);
+            return (ICommand) AppCtx.Resolve(name);
+        }
+    }
+
+    
+}
---
+++ AIMLSpringBot/Commands/Especific/CommandDiagnostico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AIMLSpringBot.Commands.Especific
+{
+    public class CommandDiagnostico: ICommand
+    {
+        public string Execute(string input)
+        {
+            return "soy diagnostico command procesando tu input: " + input;
+        }
+    }
+}
